d8193ed9e745a346c548225ef3dc5ceeae525d2f
unknown <Flexxo@FLX-PMB-301-005.LABFLEXXO.LOCAL>
2024-08-10 16:40:59 -0300

    feito novas modificações

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,11 +16,33 @@
         <div class="d-grid gap-2 d-md-flex justify-content-md-start">
             <a href="/contato" class="btn btn-primary btn-lg px-4 me-md-2">Agende seu horario</a>
         </div>
-
     </div>
 </div>
 
+<div class="row">
 
+    <div id="carouselExample" class="carousel slide">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="imagem/barba.jpg" class="d-block w-100" alt="1">
+            </div>
+            <div class="carousel-item">
+                <img src="imagem/corte.jpg" class="d-block w-100" alt="2">
+            </div>
+            <div class="carousel-item">
+                <img src="imagem/salao.jpeg" class="d-block w-100" alt="3">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Anterior</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Proxima</span>
+        </button>
+    </div>
+</div>
 
 
 @endsection
